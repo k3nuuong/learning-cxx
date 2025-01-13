76d1415318105cf3fc07b215aefdd9966f3f7d4a
k3n <k3nwong@qq.com>
2025-01-13 22:27:13 +0800

    finish learn 3-7

--- exercises/04_static/main.cpp
+++ exercises/04_static/main.cpp
@@ -2,7 +2,9 @@
 
 // READ: `static` 关键字 <https://zh.cppreference.com/w/cpp/language/storage_duration>
 // THINK: 这个函数的两个 `static` 各自的作用是什么？
+// static int func(): 这里的 static 修饰函数，作用是使这个函数的链接是内部的 (internal linkage)，这个函数只能在定义它的文件中使用
 static int func(int param) {
+    // static修饰局部变量，使该变量在函数调用之间保持其值，其跨调用是持久的
     static int static_ = param;
     // std::cout << "static_ = " << static_ << std::endl;
     return static_++;
@@ -10,10 +12,10 @@ static int func(int param) {
 
 int main(int argc, char **argv) {
     // TODO: 将下列 `?` 替换为正确的数字
-    ASSERT(func(5) == ?, "static variable value incorrect");
-    ASSERT(func(4) == ?, "static variable value incorrect");
-    ASSERT(func(3) == ?, "static variable value incorrect");
-    ASSERT(func(2) == ?, "static variable value incorrect");
-    ASSERT(func(1) == ?, "static variable value incorrect");
+    ASSERT(func(5) == 5, "static variable value incorrect");
+    ASSERT(func(4) == 6, "static variable value incorrect");
+    ASSERT(func(3) == 7, "static variable value incorrect");
+    ASSERT(func(2) == 8, "static variable value incorrect");
+    ASSERT(func(1) == 9, "static variable value incorrect");
     return 0;
 }
--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -1,14 +1,32 @@
 #include "../exercise.h"
 
+/*
+constexpr用于声明常量表达式,可以在编译时求值的表达式. 
+当函数为递归函数时,递归调用constexpr函数时,constexpr编译器会超过默认的操作计数限制,导致出错
+*/
+
+
+
+// constexpr unsigned long long fibonacci(int i) {
+//     switch (i) {
+//         case 0:
+//             return 0;
+//         case 1:
+//             return 1;
+//         default:
+//             return fibonacci(i - 1) + fibonacci(i - 2);
+//     }
+// }
+
+// 非递归调用版本
 constexpr unsigned long long fibonacci(int i) {
-    switch (i) {
-        case 0:
-            return 0;
-        case 1:
-            return 1;
-        default:
-            return fibonacci(i - 1) + fibonacci(i - 2);
+    unsigned long long a = 0, b = 1;
+    for (int j = 0; j < i; ++j) {
+        unsigned long long temp = a;
+        a = b;
+        b = temp + b;
     }
+    return a;
 }
 
 int main(int argc, char **argv) {
@@ -18,6 +36,7 @@ int main(int argc, char **argv) {
 
     // TODO: 观察错误信息，修改一处，使代码编译运行
     // PS: 编译运行，但是不一定能算出结果……
+    // error: 05_constexpr/main.cpp:16:54: error: ‘constexpr’ evaluation operation count exceeds limit of 33554432 (use ‘-fconstexpr-ops-limit=’ to increase the limit)
     constexpr auto ANS_N = 90;
     constexpr auto ANS = fibonacci(ANS_N);
     std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
